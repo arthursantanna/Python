750a920f098cda9f0d13b201b82957a5c4096b07
Arthur <101467349+arthursantanna@users.noreply.github.com>
2023-11-23 11:22:07 -0300

    Atualizando menu

--- banco.py
+++ banco.py
@@ -131,6 +131,106 @@ def cadastro_cliente(dados_cliente):
     input("\nCadastro realizado! Pressione ENTER voltar ao menu...")
     return dados_cliente
 
+    def gerar_conta():
+        # randint -> Return a random integer N such that a <= N <= b. Alias for randrange(a, b+1).
+        conta = randint(1000, 9999)
+        return conta
+
+    # aqui nessa parte eu adiciono o número da conta na lista com os dados do cliente:
+    dados_cliente.append(gerar_conta())
+
+    def conta():
+        while True: 
+
+            limpar_tela()
+
+            print("Mack Bank - Cadastro de Conta\n")
+            print(f"Número da Conta: {dados_cliente[0]}")
+
+            # nome do cliente
+            if not dados_cliente[1]: 
+                print(f"Nome do Cliente: {dados_cliente[1]}")
+            else:
+                nome = input("Nome do Cliente: ")
+
+                if nome.strip() == "": 
+                    mensagem = "O nome do cliente precisa ser preenchido."
+
+                elif nome.strip().isnumeric(): 
+                    mensagem = "O nome do cliente não pode ser numérico."
+
+                else:
+                    dados_cliente.append(nome)
+
+                    if dados_cliente[2] != None:
+                        print(f"Telefone.......: {dados_cliente[2]}")
+                    else:
+                        telefone = input("Telefone.......: ")
+                        if telefone.isnumeric() == False:
+                            print("Somente números são permitidos!")
+                        else:
+                            int(telefone)
+                            dados_cliente.append(telefone)
+                            if dados_cliente[3] != None:
+                                print(f"E-mail..........: {dados_cliente[3]}")
+                            else:
+                                email = input("E-mail..........: ")
+                                email = email.strip()
+                                if not "@" in email or not "." in email: 
+                                    print("Email Inválido! Pressione alguma tecla para prosseguir...")
+                                else:
+                                    dados_cliente.append(email)
+
+            # saldo inicial
+            try:
+                print(f"Saldo Inicial...: R$ {dados_cliente[4]}")
+            except:
+                try:
+                    saldo_inical = float(input("Saldo Inicial...: R$ "))
+                    if saldo_inical < 1000: raise 
+                    dados_cliente.append(saldo_inical)
+                except:
+                    print("O saldo inicial precisa ser numérico e maior que 1000. Pressione alguma tecla para prosseguir...") 
+                    msvcrt.getch()
+                    continue
+
+            # limite de crédito
+            try:
+                print(f"Limite de Crédito: R$ {dados_cliente[5]}")
+            except:
+                try:
+                    limite_credito = float(input("Limite de Crédito: R$ "))
+                    if limite_credito < 0: raise
+                    dados_cliente.append(limite_credito)
+                except:
+                    print("O valor precisa ser numérico e maior ou igual a 0. Pressione alguma tecla para prosseguir...")
+                    msvcrt.getch()
+                    continue
+            # senha
+            senha = input(f"Senha............: ")
+
+            if len(senha) != 6: 
+                print("A senha precisa ter 6 caracteres. Pressione alguma tecla para prosseguir...")
+                msvcrt.getch()
+                continue
+            else: 
+                confirmacao_senha = input("Repita a Senha...: ")
+                if senha != confirmacao_senha: 
+                    print("As senhas precisam ser iguais! Pressione alguma tecla para prosseguir...")
+                    msvcrt.getch()
+                    continue
+                else:
+                    dados_cliente.append(senha)
+
+                    tentativa_senha = 1
+                    bloqueado = False
+                    dados_cliente.append(tentativa_senha)
+                    dados_cliente.append(bloqueado)
+                    break
+    conta()
+    input("\nCadastro realizado! Pressione ENTER voltar ao menu...")
+    return dados_cliente
+
 def deposito(dados_cliente, saques_depositos):
 
     limpar_tela()
@@ -317,62 +417,60 @@ while not fim:
 
     opcao = int(input("Sua opção: "))
 
-    match opcao:
-
         # cadastro
-        case 1:
-            if not cadastro_preenchido:
-                limpar_tela()
-                dados_cliente = cadastro_cliente(dados_cliente)
-                cadastro_preenchido = True
+    if opcao == 1:
+        if not cadastro_preenchido:
+            limpar_tela()
+            dados_cliente = cadastro_cliente(dados_cliente)
+            cadastro_preenchido = True
+        else:
+            input("O cadastro já foi feito. Presione ENTER para voltar ao menu inicial.")
+    # deposito
+    elif opcao == 2:
+        if not cadastro_preenchido:
+            limpar_tela()
+            input("O cadastro ainda não foi feito. Presione ENTER para voltar ao menu inicial.")
+        else:
+            dados_cliente, saques_depositos = deposito(dados_cliente, saques_depositos)
+    # saque
+    elif opcao == 3:
+        if not cadastro_preenchido:
+            limpar_tela()
+            input("O cadastro ainda não foi feito. Presione ENTER para voltar ao menu inicial.")
+        else:
+            if not dados_cliente[8]:
+                dados_cliente, saques_depositos = saque(dados_cliente, saques_depositos)
             else:
-                input("O cadastro já foi feito. Presione ENTER para voltar ao menu inicial.")
-        # deposito
-        case 2:
-            if not cadastro_preenchido:
-                limpar_tela()
-                input("O cadastro ainda não foi feito. Presione ENTER para voltar ao menu inicial.")
-            else:
-                dados_cliente, saques_depositos = deposito(dados_cliente, saques_depositos)
-        # saque
-        case 3:
-            if not cadastro_preenchido:
-                limpar_tela()
-                input("O cadastro ainda não foi feito. Presione ENTER para voltar ao menu inicial.")
-            else:
-                if not dados_cliente[8]:
-                    dados_cliente, saques_depositos = saque(dados_cliente, saques_depositos)
-                else:
-                    input("Conta bloqueada. Presione ENTER para voltar ao menu inicial.")
+                input("Conta bloqueada. Presione ENTER para voltar ao menu inicial.")
 
-        # consulta saldo
-        case 4:
-            if not cadastro_preenchido:
-                limpar_tela()
-                input("O cadastro ainda não foi feito. Presione ENTER para voltar ao menu inicial.")
+    # consulta saldo
+    elif opcao == 4:
+        if not cadastro_preenchido:
+            limpar_tela()
+            input("O cadastro ainda não foi feito. Presione ENTER para voltar ao menu inicial.")
+        else:
+            if not dados_cliente[8]:
+                dados_cliente = consulta_saldo(dados_cliente)
             else:
-                if not dados_cliente[8]:
-                    dados_cliente = consulta_saldo(dados_cliente)
-                else:
-                    input("Conta bloqueada. Presione ENTER para voltar ao menu inicial.")
+                input("Conta bloqueada. Presione ENTER para voltar ao menu inicial.")
 
-        # consulta extrato
-        case 5:
-            if not cadastro_preenchido:
-                limpar_tela()
-                input("O cadastro ainda não foi feito. Presione ENTER para voltar ao menu inicial.")
+    # consulta extrato
+    elif opcao == 5:
+        if not cadastro_preenchido:
+            limpar_tela()
+            input("O cadastro ainda não foi feito. Presione ENTER para voltar ao menu inicial.")
+        else:
+            if not dados_cliente[8]:
+                dados_cliente, saques_depositos = consultar_extrato(dados_cliente, saques_depositos)
             else:
-                if not dados_cliente[8]:
-                    dados_cliente, saques_depositos = consultar_extrato(dados_cliente, saques_depositos)
-                else:
-                    input("Conta bloqueada. Presione ENTER para voltar ao menu inicial.")
-        # finalizar
-        case 6:
-            fim = True
-        # opção inválida
-        case _:
-            print("\nDigite uma opção válida! Pressine alguma tecla para voltar ao menu...")
-            msvcrt.getch()
+                input("Conta bloqueada. Presione ENTER para voltar ao menu inicial.")
+    # finalizar
+    elif opcao == 6:
+        fim = True
+    # opção inválida
+    else:
+        print("\nDigite uma opção válida! Pressine alguma tecla para voltar ao menu...")
+        msvcrt.getch()
 
 limpar_tela()
 print("""MACK BANK – SOBRE
